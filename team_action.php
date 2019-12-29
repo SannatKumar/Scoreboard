9be2b8e826d85ac873b6f923537cb7d48e2ea21e
Raj Kumar <interplaytheheads@gmail.com>
2019-12-29 22:10:44 +0200

    script to insert team name into database

--- team_action.php
+++ team_action.php
@@ -1,21 +1,7 @@
 <?php
 
-    //Database credentials
-    $servername = "localhost";
-  	$dusername = "root";
-  	$dpassword = "";
-  	$dbname = "scoreboard";
-
-  	try {
-      		$connect = new PDO("mysql:host=$servername; dbname=$dbname", $dusername, $dpassword);
-      		// set the PDO error mode to exception
-      		$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      		//echo "Connected successfully";
-      	}
-  	catch(PDOException $e) {
-      		echo "Connection failed: " . $e->getMessage();
-          }
-
+// this script takes the team name to the database and redirects the page
+	require 'config.php';
 	//Assign the value from the dashboard page to the database
 	$gameid = 1;
 	$firstTeam = $_POST['teama'];
@@ -30,6 +16,8 @@
 	$queryHandle->bindParam(2, $firstTeam);
 	$queryHandle->bindParam(3, $secondTeam);
 	$queryHandle->execute();
+
+	// redirect the page to displauyscoreboard
 	header("Location: displayscoreboard.php");
 
 ?>
