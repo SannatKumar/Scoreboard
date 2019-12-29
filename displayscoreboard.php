73566b2a4e3a179e2898e913bb21948f1e3bab38
Raj Kumar <interplaytheheads@gmail.com>
2019-12-29 22:12:08 +0200

    Scoreboard UI

--- displayscoreboard.php
+++ displayscoreboard.php
@@ -1,44 +1,40 @@
 <?php
 
-//Database credentials
-$servername = "localhost";
-$dusername = "root";
-$dpassword = "";
-$dbname = "scoreboard";
-
-try {
-    $connect = new PDO("mysql:host=$servername; dbname=$dbname", $dusername, $dpassword);
-    // set the PDO error mode to exception
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
+require 'config.php';
+//sql query assign and execution 
 $gameQuery = "SELECT * FROM game";
 $queryHandle = $connect->prepare($gameQuery);
 $queryHandle->execute();
+
+//fetch the data into the php variable called row
 $row = $queryHandle->fetch();
+
+//assign the data from database into the php variables
 $teamA = $row['teama'];
 $teamB = $row['teamb'];
 
 
-
+//sql query assign and execution
 $userQueryString = "SELECT * FROM gamedetails";
 $queryHandle = $connect->prepare($userQueryString);
 $queryHandle->execute();
+
+//initialize variables 
 $teamAtotalscore = 0;
 $teamBtotalscore = 0;
 $avalue = 0;
 $bvalue = 0;
+// initilaize arrays
 $teamAplayer = array();
 $teamBplayer = array();
 $teamAscore = array();
 $teamBscore = array();
+
+//fetching the data using while loop
 while ($row = $queryHandle->fetch()) {
     $teamName = $row['team'];
 
-
+    // string compare on lower case player and get player and score list
     if ((strtolower($teamName)) == (strtolower($teamA))) {
         $teamAtotalscore++;
         $teamAplayer[] = $row['player'];
@@ -48,6 +44,8 @@ while ($row = $queryHandle->fetch()) {
         $teamBplayer[] = $row['player'];
         $teamBscore[] = $row['score'];
     }
+
+    // function to add the array
     $scorea = 0;
     $scorea = array_sum($teamAscore);
     $scoreb = 0;
@@ -61,8 +59,6 @@ while ($row = $queryHandle->fetch()) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap4.min.css" rel="stylesheet"/>
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
@@ -116,6 +112,8 @@ while ($row = $queryHandle->fetch()) {
 </head>
 
 <body>
+
+<!-- UI header -->
     <header id="header">
         <h1 style="text-align: center;">Scoreboard</h1>
         <div class="container">
@@ -143,18 +141,8 @@ while ($row = $queryHandle->fetch()) {
             </div>
     </div>
     </header>
-   <!-- <form action="displayscoreboard.php" method="post">
-    <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <button type="submit"  value="asc" name ="asc"  class="btn btn-primary" id="asc">Ascending</button>
-                    </div>
-                    <div class="col">
-                        <button type="submit" value="desc" name = "desc" class="btn btn-primary" id="desc">Descending</button>
-                    </div>
-                </div>
-                    </div>
-    </form>-->
+
+    <!-- Table UI -->
     <div class="container px-lg-5">
         <div class="row mx-lg-n5">
             <div class="col py-3 px-lg-5 border bg-light">
@@ -164,11 +152,12 @@ while ($row = $queryHandle->fetch()) {
                             <th scope="col" >S.no</th>
                             <th scope="col">Team Name</th>
                             <th scope="col">Player name</th>
-                            <th scope="col" >Score <i class="fa fa-arrow-down" aria-hidden="true"></i></th>
+                            <th scope="col" >Score </th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
+                        // php display team B data
                               $Snvalue = 1;
                                 foreach ($teamAplayer as $player) {
                                     echo '<tr>';
@@ -197,6 +186,7 @@ while ($row = $queryHandle->fetch()) {
                     </thead>
                     <tbody>
                         <?php
+                        // php display team B data
                         $bsnvalue = 1;
                         foreach ($teamBplayer as $player) {
                             echo '<tr>';
@@ -214,6 +204,8 @@ while ($row = $queryHandle->fetch()) {
             </div>
         </div>
     </div>
+
+    <!--UI to take the input field and submit the data to action page via post method-->
     <form action="action_page.php" method="post">
         <div class="form-group">
             <div class="container px-lg-5">
@@ -247,42 +239,12 @@ while ($row = $queryHandle->fetch()) {
             </div>
         </div>
     </form>
-
-    
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/dataTables.bootstrap4.min.js"></script>
-    
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function(){
-            $('.fa-arrow-down').click(function(){
-                $.ajax({
-				url:'get_order_data.php',
-				type:'post',
-				data:{'column':getColumnName,'sortOrder':splitOrder},
-				success: function(response){
-					if(splitOrder == 'asc')
-					{
-						getSortHeading.attr('id',splitIDName+'-desc');
-					}
-					else
-					{
-						getSortHeading.attr('id',splitIDName+'-asc');
-					}	
-					
-					$(".table tr:not(:first)").remove();
-					$(".table").append(response);
-				}
-			});
-            })
-        })
-    </script>
-</body>
 
+</body>
 </html>
